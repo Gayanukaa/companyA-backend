f3205ddd03c8dc3192a6f040187ae6da6b9b2050
Gayanuka Amarasuriya <gayanukaamarasuriya@gmail.com>
2024-04-03 18:50:22 +0530

    Test Application file

--- backend/src/main/java/com/companyA/backend/BackendApplication.java
+++ backend/src/main/java/com/companyA/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package com.companyA.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BackendApplication {
 
 	public static void main(String[] args) {
---
+++ backend/src/main/java/com/companyA/backend/GeneralManagementSystem/GeneralManagementApplication.java
@@ -0,0 +1,13 @@
+package com.companyA.backend.GeneralManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.companyA.backend.InventoryManagementSystem.InventoryApplication.class, args);
+    }
+
+}
