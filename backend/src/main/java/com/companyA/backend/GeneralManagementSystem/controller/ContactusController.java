b23322e48cdf1fcfb9fd360e42206fc3546b09d8
abrajapaksha <155985426+abrajapaksha@users.noreply.github.com>
2024-04-09 23:02:13 +0530

    Update ContactusController.java
    ContactUs

--- backend/src/main/java/com/companyA/backend/GeneralManagementSystem/controller/ContactusController.java
+++ backend/src/main/java/com/companyA/backend/GeneralManagementSystem/controller/ContactusController.java
@@ -1,4 +1,27 @@
 package com.companyA.backend.GeneralManagementSystem.controller;
 
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/contactus")
+@AllArgsConstructor
+
 public class ContactusController {
+    //customer feedback
+    @PostMapping("/feedback")
+    public ResponseEntity<Map<String,String>>customerFeedbackControl(@Valid @RequestBody Contactus contactus ){
+        String feedbackdetails = contactusServise.customerFeedback(contactus);
+        Map<String, String> response = new HashMap<>();
+        response.put("message",feedbackdetails);
+    }
+
 }
