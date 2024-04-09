5305d36a5ab22294ecd35defb498b5b2f2f8b54c
Malitha Prabhashana <malithaprabhashana@gmail.com>
2024-04-09 08:54:02 +0530

    Request Body Validation

--- backend/src/main/java/com/companyA/backend/GeneralManagementSystem/controller/CustomerController.java
+++ backend/src/main/java/com/companyA/backend/GeneralManagementSystem/controller/CustomerController.java
@@ -3,13 +3,13 @@ package com.companyA.backend.GeneralManagementSystem.controller;
 import com.companyA.backend.GeneralManagementSystem.model.Customer;
 import com.companyA.backend.GeneralManagementSystem.model.User;
 import com.companyA.backend.GeneralManagementSystem.service.CustomerService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,11 +23,33 @@ public class CustomerController {
     private CustomerService customerService;
 
     @PostMapping("/register")
-    public ResponseEntity<Map <String, String>> customerRegisterControl(@RequestBody Customer customer) {
+    public ResponseEntity<Map <String, String>> customerRegisterControl(@Valid @RequestBody Customer customer) {
         String registrationResult = customerService.customerRegister(customer);
 
         Map<String, String> response = new HashMap<>();
         response.put("message", registrationResult);
-        return ResponseEntity.ok(response);
+
+        if (registrationResult.equals("Email already exists")) {
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+    }
+
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
     }
 }
--- backend/src/main/java/com/companyA/backend/GeneralManagementSystem/model/User.java
+++ backend/src/main/java/com/companyA/backend/GeneralManagementSystem/model/User.java
@@ -1,5 +1,6 @@
 package com.companyA.backend.GeneralManagementSystem.model;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 
@@ -10,10 +11,18 @@ public class User {
     @Id
     private String id;
 
+    @NotBlank(message = "FirstName is Required")
     private String firstName;
-    private String lastName;
-    private String mobileNumber;
-    private String email;
-    private String password;
 
+    @NotBlank(message = "LastName is Required")
+    private String lastName;
+
+    @NotBlank(message = "Mobile Number is Required")
+    private String mobileNumber;
+
+    @NotBlank(message = "Email is Required")
+    private String email;
+
+    @NotBlank(message = "Password is Required")
+    private String password;
 }
