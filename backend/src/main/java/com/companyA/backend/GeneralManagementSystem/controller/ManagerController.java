196c0c6ec6d68ca83f91f7f130368d5bbeaaef84
TharinduPrabath2001 <161202116+TharinduPrabath2001@users.noreply.github.com>
2024-04-10 07:46:09 +0530

    Manager Controls Modified
    New API for viewing all managers.
    No duplicate emails.
    New API for update manager details.

--- backend/src/main/java/com/companyA/backend/GeneralManagementSystem/controller/ManagerController.java
+++ backend/src/main/java/com/companyA/backend/GeneralManagementSystem/controller/ManagerController.java
@@ -1,11 +1,13 @@
 package com.companyA.backend.GeneralManagementSystem.controller;
 
+import com.companyA.backend.GeneralManagementSystem.DTO.ManagerDTO;
 import com.companyA.backend.GeneralManagementSystem.model.Manager;
 import com.companyA.backend.GeneralManagementSystem.service.ManagerService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -18,10 +20,22 @@ public class ManagerController {
     @PostMapping("/createaccount")
     public String createManagerHandle(@RequestBody Manager manager) {
         return managerService.createManager(manager);
+
     }
 
     @GetMapping("/findById")
     public Optional<Manager> findManagerById(@RequestParam("id") String id){
         return managerService.findManagerById(id);
     }
+
+    @PostMapping("/updateById")
+    public String updateManager(@RequestBody Manager manager){
+        return managerService.updatemanagerById(manager);
+    }
+
+    @GetMapping("/viewAllManagers")
+    public List<ManagerDTO> viewAllManagers(){
+        return managerService.viewAllManagers();
+    }
+
 }
--- backend/src/main/java/com/companyA/backend/GeneralManagementSystem/service/ManagerService.java
+++ backend/src/main/java/com/companyA/backend/GeneralManagementSystem/service/ManagerService.java
@@ -1,10 +1,13 @@
 package com.companyA.backend.GeneralManagementSystem.service;
 
+import com.companyA.backend.GeneralManagementSystem.DTO.ManagerDTO;
 import com.companyA.backend.GeneralManagementSystem.model.Manager;
 import com.companyA.backend.GeneralManagementSystem.repository.ManagerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,12 +17,45 @@ public class ManagerService {
     private ManagerRepository managerRepository;
 
     public String createManager(Manager manager) {
-        managerRepository.save(manager);
-        return "Manager added Successfully";
+        String tempEmail = manager.getEmail();
+        Manager ExistingManager = managerRepository.findByEmail(tempEmail);
+        if (ExistingManager!=null) {
+            String tempRoll = ExistingManager.getRole();
+            return "The personnel with the specific email is already assigned to "+tempRoll+".";
+        }
+        else {
+            managerRepository.save(manager);
+            return "Manager added Successfully";
+        }
+
     }
 
     public Optional<Manager> findManagerById(String managerId) {
         return managerRepository.findById(managerId);
+    }
+
+    public String updatemanagerById(Manager manager) {
+        String managerId = manager.getId();
+        managerRepository.deleteById(managerId);
+        managerRepository.save(manager);
+        return "Manager updated successfully.";
+
+    }
+
+    public List<ManagerDTO> viewAllManagers(){
+        List<Manager> managers = managerRepository.findAll();
+        List<ManagerDTO> managerDTOs = new ArrayList<>();
+        for (Manager manager : managers) {
+            ManagerDTO dto = new ManagerDTO();
+            dto.setId(manager.getId());
+            dto.setMobileNumber(manager.getMobileNumber());
+            dto.setEmail(manager.getEmail());
+            dto.setRole(manager.getRole());
+            dto.setFirstName(manager.getFirstName());
+            dto.setLastName(manager.getLastName());
+            managerDTOs.add(dto);
+        }
+        return managerDTOs;
     }
 
 }
