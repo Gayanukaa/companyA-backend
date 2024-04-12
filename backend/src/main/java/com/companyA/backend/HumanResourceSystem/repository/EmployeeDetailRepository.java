ee7ca8a49658dbf4eabf3ac6ed89c5176f5537c1
Ravindu Rukmal <ravindurukmal44@gmail.com>
2024-04-12 13:43:24 +0530

    Employee Repoistory Added

---
+++ backend/src/main/java/com/companyA/backend/HumanResourceSystem/repository/EmployeeDetailRepository.java
@@ -0,0 +1,13 @@
+package com.companyA.backend.HumanResourceSystem.repository;
+
+import com.companyA.backend.HumanResourceSystem.model.EmployeeDetailModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeDetailRepository extends MongoRepository<EmployeeDetailModel,String> {
+    EmployeeDetailModel findByEmployeeId(String employeeId);
+    EmployeeDetailModel findByEmployeeName(String employeeName);
+}
