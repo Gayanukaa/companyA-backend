a3c991a2782b0b0a2371d7f3f87385f698e09a31
yasaspathmila <pathmilakky.21@uom.lk>
2024-04-12 23:44:29 +0530

    Adding 'Sample' class and 'Prototype' class
    Samples class and prototype classes were created by extending the  TestSubjects class.

---
+++ backend/src/main/java/com/companyA/backend/QualityAssuaranceSystem/model/Prototype.java
@@ -0,0 +1,12 @@
+package com.companyA.backend.QualityAssuaranceSystem.model;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
+
+public class Prototype extends TestSubjects {
+    private static final String testSubjectType = "Prototype" ;
+
+}
---
+++ backend/src/main/java/com/companyA/backend/QualityAssuaranceSystem/model/Sample.java
@@ -0,0 +1,12 @@
+package com.companyA.backend.QualityAssuaranceSystem.model;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
+
+public class Sample extends TestSubjects {
+    private static final String testSubjectType = "Sample" ;
+
+}
