a7a4849eb7f7051b889454783b87d5109d8e4e42
AnupaAlwis <isurajithalwis@gmail.com>
2024-04-16 13:33:22 +0530

    Inventory Manager Class Update

--- backend/src/main/java/com/companyA/backend/InventoryManagementSystem/model/InventoryManager.java
+++ backend/src/main/java/com/companyA/backend/InventoryManagementSystem/model/InventoryManager.java
@@ -1,5 +1,6 @@
 package com.companyA.backend.InventoryManagementSystem.model;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,19 +10,19 @@ import org.springframework.data.mongodb.core.mapping.DocumentReference;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
 
 @Document(collection = "InventoryManager")
 @Data
 @Getter
 @Setter
 public class InventoryManager {
-    @Id
+    @Id @NotBlank
     private String managerId;
     private String managerName;
     private String managerEmail;
     private String managerPhone;
     @DocumentReference
-    private List<Warehouse> warehouses; // List to store warehouses offered
-    @DocumentReference
-    private List<Suppliers> listOfSuppliers; // List to store list of suppliers offered
+    private Map<String, String> warehouses; // List to store warehouses offered
+
 }
