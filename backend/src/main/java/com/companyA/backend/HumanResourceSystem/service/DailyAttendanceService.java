ba890603e90b6db571d061106517ecedde2a6db2
rukmalrrl <ravindurukmal44@gmail.com>
2024-04-16 15:57:17 +0530

    Daily Attendance Service added

---
+++ backend/src/main/java/com/companyA/backend/HumanResourceSystem/service/DailyAttendanceService.java
@@ -0,0 +1,53 @@
+package com.companyA.backend.HumanResourceSystem.service;
+
+import com.companyA.backend.HumanResourceSystem.model.DailyAttendanceModel;
+import com.companyA.backend.HumanResourceSystem.repository.DailyAttendanceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class DailyAttendanceService {
+
+    @Autowired
+    DailyAttendanceRepository dailyAttendanceRepository;
+
+    public void markSignInAndAttendance(String id){
+        DailyAttendanceModel dailyAttendance = new DailyAttendanceModel();
+        dailyAttendance.setId(id);
+        dailyAttendance.setPresent(true);
+        dailyAttendance.setDate(LocalDate.now());
+        dailyAttendance.setSignInTime(LocalDateTime.now());
+        dailyAttendanceRepository.save(dailyAttendance);
+    }
+    public void markSignOut(String id){
+        DailyAttendanceModel dailyAttendance = dailyAttendanceRepository.findByIdAndDate(id,LocalDate.now());
+        if (dailyAttendance.isPresent()){
+            DailyAttendanceModel attendance = dailyAttendance;
+            attendance.setSignOutTime(LocalDateTime.now());
+            dailyAttendanceRepository.save(attendance);
+
+        }
+        else{
+            throw new RuntimeException("Attendance not found for employee with id: " + id);
+        }
+    }
+
+    public List<DailyAttendanceModel> getWeeklyAttendace(String id){
+        LocalDate startDate = LocalDate.now().minusDays(7);
+        LocalDate endDate = LocalDate.now();
+
+        return dailyAttendanceRepository.findByEmployeeIdAndDateBetween(id, startDate, endDate);
+
+    }
+    public List<DailyAttendanceModel> getDailyAttendance(LocalDate date){
+
+        return dailyAttendanceRepository.findByDate(date);
+
+    }
+}
+
+
