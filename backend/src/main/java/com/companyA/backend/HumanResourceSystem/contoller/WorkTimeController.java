0144b523aa0abb0004999137e657614d7526cda2
Dunishka <162004328+Dunishka@users.noreply.github.com>
2024-04-16 12:25:56 +0530

    add user signed in time and signed out time to the database for each day

---
+++ backend/src/main/java/com/companyA/backend/HumanResourceSystem/contoller/WorkTimeController.java
@@ -0,0 +1,41 @@
+package com.companyA.backend.HumanResourceSystem.contoller;
+
+import com.companyA.backend.HumanResourceSystem.model.EmployeeDetailModel;
+import com.companyA.backend.HumanResourceSystem.model.WorkTimeModel;
+import com.companyA.backend.HumanResourceSystem.repository.WorkTimeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping(value = "/WorkTime")
+
+public class WorkTimeController {
+
+    @Autowired
+    WorkTimeRepository workTimeRepository;
+
+    @PostMapping("/add")
+    public ResponseEntity<List<WorkTimeModel>> insertWorkTime(@RequestBody List<WorkTimeModel> workTimeModels) {
+        for (WorkTimeModel workTimeModel : workTimeModels) {
+            List<WorkTimeModel> existingWorkTime = workTimeRepository.findByIdAndDate(workTimeModel.getId(), workTimeModel.getDate());
+
+            if (existingWorkTime.isEmpty()) {
+                workTimeRepository.save(workTimeModel);
+            }
+            else {
+                workTimeRepository.save(workTimeModel);
+            }
+        }
+        return new ResponseEntity<>(workTimeModels, HttpStatus.CREATED);
+    }
+}
