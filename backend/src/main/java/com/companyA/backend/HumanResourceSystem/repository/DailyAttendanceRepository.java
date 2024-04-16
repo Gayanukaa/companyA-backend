723924c2b427be53da133189f4000786407a1cd3
rukmalrrl <ravindurukmal44@gmail.com>
2024-04-16 15:57:30 +0530

    Daily Attendance Repository added

--- backend/src/main/java/com/companyA/backend/HumanResourceSystem/repository/DailyAttendanceRepository.java
+++ backend/src/main/java/com/companyA/backend/HumanResourceSystem/repository/DailyAttendanceRepository.java
@@ -3,6 +3,13 @@ package com.companyA.backend.HumanResourceSystem.repository;
 import com.companyA.backend.HumanResourceSystem.model.DailyAttendanceModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface DailyAttendanceRepository extends MongoRepository<DailyAttendanceModel,String> {
+import java.time.LocalDate;
+import java.util.List;
 
+public interface DailyAttendanceRepository extends MongoRepository<DailyAttendanceModel,String> {
+    DailyAttendanceModel findByIdAndDate(String id, LocalDate date);
+
+    List<DailyAttendanceModel> findByEmployeeIdAndDateBetween(String id, LocalDate startDate, LocalDate endDate);
+
+    List<DailyAttendanceModel> findByDate(LocalDate date);
 }
