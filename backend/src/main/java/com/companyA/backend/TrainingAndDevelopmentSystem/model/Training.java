f738fe3caf2220061f380fcf7528091e274bef1e
dnmanamperi <dnmanamperi@gmail.com>
2024-04-18 00:13:18 +0530

    add training model and repo

---
+++ backend/src/main/java/com/companyA/backend/TrainingAndDevelopmentSystem/model/Training.java
@@ -0,0 +1,36 @@
+package com.companyA.backend.TrainingAndDevelopmentSystem.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
+public class Training {
+    @Id
+    private String id;
+    private int duration ;
+    private double cost;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
