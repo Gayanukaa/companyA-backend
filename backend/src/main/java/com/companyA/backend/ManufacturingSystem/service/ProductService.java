000b98c1d32926fbd62e2f85db52bc1ce8356332
Naiya21 <tharindusasankabc@gmail.com>
2024-04-19 22:47:44 +0530

    run error correction

--- backend/src/main/java/com/companyA/backend/ManufacturingSystem/service/ProductService.java
+++ backend/src/main/java/com/companyA/backend/ManufacturingSystem/service/ProductService.java
@@ -2,12 +2,12 @@ package com.companyA.backend.ManufacturingSystem.service;
 
 import com.companyA.backend.ManufacturingSystem.model.Product;
 import com.companyA.backend.ManufacturingSystem.repository.ProductRepository;
-import lombok.AllArgsConstructor;
+
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-@AllArgsConstructor
+
 public class ProductService {
     private final ProductRepository productRepository;
 
