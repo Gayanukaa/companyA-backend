65dd95654fdb5810a867a1d8e5bec3129f01097c
Dunishka <162004328+Dunishka@users.noreply.github.com>
2024-04-19 22:14:40 +0530

    Do the calculation part of the pay work hours and OT hours.

---
+++ backend/src/main/java/com/companyA/backend/HumanResourceSystem/service/PayRollService.java
@@ -0,0 +1,81 @@
+package com.companyA.backend.HumanResourceSystem.service;
+
+import com.companyA.backend.HumanResourceSystem.model.PayrollModel;
+import com.companyA.backend.HumanResourceSystem.model.DailyAttendanceModel;
+import com.companyA.backend.HumanResourceSystem.repository.PayRollRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@Service
+public class PayRollService {
+
+    @Autowired
+    static PayRollRepository payRollRepository;
+
+    public String calculateWorkHours(List<DailyAttendanceModel> workTimeList) {
+        long workMinutes = 0;
+
+        for (DailyAttendanceModel workTime : workTimeList) {
+            LocalDateTime signInTime = workTime.getTimeSignedIn();
+            LocalDateTime signOutTime = workTime.getTimeSignedOut();
+
+            if (signInTime != null && signOutTime != null) {
+                long minutes = ChronoUnit.MINUTES.between(signInTime, signOutTime);
+
+                workMinutes += minutes ;
+
+            }
+        }
+
+        long wHours = workMinutes / 60;
+        long wMinutes = workMinutes % 60;
+
+
+
+        return "Total worked time: "+ wHours + " hours " +   wMinutes + " minutes" ;
+
+    }
+
+
+    public String calculaOtHours(List<DailyAttendanceModel> workTimeList) {
+        long workMinutes = 0;
+        long otMinutes = 0;
+
+        for (DailyAttendanceModel workTime : workTimeList) {
+            LocalDateTime signInTime = workTime.getTimeSignedIn();
+            LocalDateTime signOutTime = workTime.getTimeSignedOut();
+
+            if (signInTime != null && signOutTime != null) {
+                long minutes = ChronoUnit.MINUTES.between(signInTime, signOutTime);
+                if (minutes > 8*60) {
+                    workMinutes += 8*60 ;
+                    otMinutes += minutes - 8*60;
+                }
+                else {
+                    workMinutes += minutes;
+                }
+            }
+        }
+
+
+        long hoursOt = otMinutes / 60;
+        long minutesOt = otMinutes % 60;
+
+
+        return "Total OT time: " + hoursOt + " Hours " + minutesOt + " minutes" ;
+
+    }
+
+
+}
+
+
