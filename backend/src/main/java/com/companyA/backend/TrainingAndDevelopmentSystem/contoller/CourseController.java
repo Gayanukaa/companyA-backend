76f980207d6ab07b512991dd7f062bc1eefb4f74
dnmanamperi <dnmanamperi@gmail.com>
2024-04-20 00:58:57 +0530

    add get and post mapping - Course controller

---
+++ backend/src/main/java/com/companyA/backend/TrainingAndDevelopmentSystem/contoller/CourseController.java
@@ -0,0 +1,34 @@
+package com.companyA.backend.TrainingAndDevelopmentSystem.contoller;
+
+import com.companyA.backend.TrainingAndDevelopmentSystem.exception.UserNotFoundException;
+import com.companyA.backend.TrainingAndDevelopmentSystem.model.Course;
+import com.companyA.backend.TrainingAndDevelopmentSystem.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class CourseController {
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @PostMapping("/api/tms/course")
+    Course newCourse(@RequestBody Course newCourse){
+        return courseRepository.save(newCourse);
+    }
+
+    @GetMapping("/api/tms/courses")
+    List<Course> getAllUsers(){
+        return courseRepository.findAll();
+    }
+
+    @GetMapping("/api/tms/courses/{id}")
+    Course getUserById(@PathVariable String id){
+        return courseRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
+    }
+
+
+}
