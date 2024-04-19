f2971065acc44cb3e8d962ff3196c57bd61da0da
dnmanamperi <dnmanamperi@gmail.com>
2024-04-20 02:34:24 +0530

    add main supervisor controller
    update model and repo

--- backend/src/main/java/com/companyA/backend/TrainingAndDevelopmentSystem/repository/MainSupervisorRepository.java
+++ backend/src/main/java/com/companyA/backend/TrainingAndDevelopmentSystem/repository/MainSupervisorRepository.java
@@ -2,6 +2,9 @@ package com.companyA.backend.TrainingAndDevelopmentSystem.repository;
 
 import com.companyA.backend.TrainingAndDevelopmentSystem.model.MainSupervisor;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface MainSupervisorRepository extends MongoRepository<MainSupervisor,String> {
+    @Query("{'mSupervisorEmail': ?0, 'mPassword': ?1}")
+    MainSupervisor findByEmailAndPassword(String email, String password);
 }
