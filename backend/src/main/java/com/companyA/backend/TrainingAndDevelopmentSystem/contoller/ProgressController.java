0832d8231fab9249016e6920dbed5b122f060fdc
dnmanamperi <dnmanamperi@gmail.com>
2024-04-20 07:23:59 +0530

    Create ProgressController.java

---
+++ backend/src/main/java/com/companyA/backend/TrainingAndDevelopmentSystem/contoller/ProgressController.java
@@ -0,0 +1,32 @@
+package com.companyA.backend.TrainingAndDevelopmentSystem.contoller;
+
+import com.companyA.backend.TrainingAndDevelopmentSystem.model.Progress;
+import com.companyA.backend.TrainingAndDevelopmentSystem.repository.ProgressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ProgressController {
+    private final ProgressRepository progressRepository;
+
+    @Autowired
+    public ProgressController(ProgressRepository progressRepository) {
+        this.progressRepository = progressRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createProgress(@RequestBody Progress progress) {
+        try {
+            progressRepository.save(progress);
+            return new ResponseEntity<>("Progress created successfully", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
