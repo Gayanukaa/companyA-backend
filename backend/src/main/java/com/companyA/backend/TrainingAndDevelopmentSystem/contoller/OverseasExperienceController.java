35abb50069a0241434ed16354a9ab7f2f73481d2
dnmanamperi <dnmanamperi@gmail.com>
2024-04-20 07:14:19 +0530

    Update OverseasExperienceController.java

--- backend/src/main/java/com/companyA/backend/TrainingAndDevelopmentSystem/contoller/OverseasExperienceController.java
+++ backend/src/main/java/com/companyA/backend/TrainingAndDevelopmentSystem/contoller/OverseasExperienceController.java
@@ -17,19 +17,40 @@ public class OverseasExperienceController {
     private OverseasExperienceRepository overseasExperienceRepository;
 
     @PostMapping("/api/tms/overseas")
-    OverseasExperience newCourse(@RequestBody OverseasExperience newCourse){
+    OverseasExperience newOSE(@RequestBody OverseasExperience newCourse){
         return overseasExperienceRepository.save(newCourse);
     }
 
     @GetMapping("/api/tms/overseas")
-    List<OverseasExperience> getAllCourses(){
+    List<OverseasExperience> getAllOSEs(){
         return overseasExperienceRepository.findAll();
     }
 
     @GetMapping("/api/tms/overseas/{id}")
-    OverseasExperience getCourseById(@PathVariable String id){
+    OverseasExperience getOSEById(@PathVariable String id){
         return overseasExperienceRepository.findById(id)
                 .orElseThrow(()->new UserNotFoundException(id));
     }
+    @DeleteMapping("/api/tms/overseas/{id}")
+    String deleteOSE(@PathVariable String id){
+        if(!overseasExperienceRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        overseasExperienceRepository.deleteById(id);
+        return "User with "+id+" has been deleted successfully";
+    }
 
+    @PutMapping("/api/tms/overseas/{id}")
+    OverseasExperience updateOSE(@RequestBody OverseasExperience newUser,@PathVariable String id){
+        return overseasExperienceRepository.findById(id)
+                .map(user -> {
+                    user.setCompanyName(newUser.getCompanyName());
+                    user.setCountry(newUser.getCountry());
+                    user.setOseId(newUser.getOseId());
+                    user.setDetails(newUser.getDetails());
+                    user.setDuration(newUser.getDuration());
+                    user.setCost(newUser.getCost());
+                    return overseasExperienceRepository.save(user);
+                }).orElseThrow(()->new UserNotFoundException(id));
+    }
 }
