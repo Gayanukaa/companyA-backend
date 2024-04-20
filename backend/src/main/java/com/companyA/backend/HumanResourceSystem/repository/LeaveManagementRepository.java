2dcd15278ef5ea2949db8a7ad26a20bb850011d9
Dunishka <162004328+Dunishka@users.noreply.github.com>
2024-04-20 11:18:45 +0530

    find the employee by employee id

---
+++ backend/src/main/java/com/companyA/backend/HumanResourceSystem/repository/LeaveManagementRepository.java
@@ -0,0 +1,14 @@
+package com.companyA.backend.HumanResourceSystem.repository;
+
+import com.companyA.backend.HumanResourceSystem.model.WorkTimeModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface LeaveManagementRepository extends MongoRepository<WorkTimeModel, String> {
+    List<WorkTimeModel> findByEmployeeId(String employeeId) ;
+}
