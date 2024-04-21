54062c1f7ca1a890cf80bfbf6b9cdb9955b6c87c
Kavishka Lakshan <kavishkalakshan527@gmail.com>
2024-04-22 03:01:40 +0530

    vehicle

--- backend/src/main/java/com/companyA/backend/LogisticsAndMaintenanceSystem/model/Vehicle.java
+++ backend/src/main/java/com/companyA/backend/LogisticsAndMaintenanceSystem/model/Vehicle.java
@@ -7,9 +7,6 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document ;
 
-
-
-
 @Document (collection =  "Vehicles")
 @AllArgsConstructor
 @NoArgsConstructor
