f6396d5c668ca3f582a41fa45eb8846ea3950412
Sulith12 <pererapsr2.21@uom.lk>
2024-04-28 00:01:33 +0530

    added finance part

---
+++ backend/src/main/java/com/companyA/backend/SalesSystem/repository/FinanceSalesTableRepository.java
@@ -0,0 +1,15 @@
+package com.companyA.backend.SalesSystem.repository;
+
+import com.companyA.backend.SalesSystem.model.CustomerData;
+import com.companyA.backend.SalesSystem.model.FinanceSalesTableBody;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface FinanceSalesTableRepository extends MongoRepository<FinanceSalesTableBody, String> {
+    Optional<FinanceSalesTableBody> findById(String _id);
+
+    FinanceSalesTableBody save(FinanceSalesTableBody data);
+}
