988ab39528099c650880fdb9b989a06fa20112b2
Sulith12 <pererapsr2.21@uom.lk>
2024-04-27 17:44:02 +0530

    changes

--- backend/src/main/java/com/companyA/backend/SalesSystem/model/SalesRecord.java
+++ backend/src/main/java/com/companyA/backend/SalesSystem/model/SalesRecord.java
@@ -3,13 +3,14 @@ package com.companyA.backend.SalesSystem.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 
 import java.util.List;
 
 @Data
 public class SalesRecord {
 
-    private String order_ID;
+    private ObjectId order_ID;
     private String order_date;
     private double order_amount;
     private List<String> components;
