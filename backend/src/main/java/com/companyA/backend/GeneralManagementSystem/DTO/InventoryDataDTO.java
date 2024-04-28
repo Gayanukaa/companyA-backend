4ff6a5144e884e53c1be747fd26b373fe91c726f
Malitha Prabhashana <malithaprabhashana@gmail.com>
2024-04-29 00:00:26 +0530

    Dashboard Component started developing

---
+++ backend/src/main/java/com/companyA/backend/GeneralManagementSystem/DTO/InventoryDataDTO.java
@@ -0,0 +1,12 @@
+package com.companyA.backend.GeneralManagementSystem.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class InventoryDataDTO {
+    private String name;
+    private Integer quantity;
+}
