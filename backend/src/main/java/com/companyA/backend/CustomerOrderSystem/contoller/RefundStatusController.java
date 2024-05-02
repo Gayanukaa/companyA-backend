797ab255fc2e69935371e81d8517b42493ee5849
hawsii <162781079+hawsii@users.noreply.github.com>
2024-05-02 10:35:03 +0530

    added @CrossOrigin notation

--- backend/src/main/java/com/companyA/backend/CustomerOrderSystem/contoller/RefundStatusController.java
+++ backend/src/main/java/com/companyA/backend/CustomerOrderSystem/contoller/RefundStatusController.java
@@ -6,10 +6,13 @@ import com.companyA.backend.CustomerOrderSystem.service.RefundStatusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
 
 
 @RestController
