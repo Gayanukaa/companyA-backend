ba37189c68a3bc2855df20bc5487b38398e6d701
samoddharmaraja <dharmarajarass.21@uom.lk>
2024-05-03 02:26:04 +0530

    disable cors for all the http methods

---
+++ backend/src/main/java/com/companyA/backend/WebConfig.java
@@ -0,0 +1,19 @@
+package com.companyA.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+				.allowedHeaders("*")
+				.allowCredentials(false)
+				.maxAge(3600);
+	}
+}
