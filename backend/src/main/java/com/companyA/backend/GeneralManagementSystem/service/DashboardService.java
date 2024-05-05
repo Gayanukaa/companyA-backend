efb399f6999a6dec285333c4f59ad6e203ccfc84
Malitha Prabhashana <malithaprabhashana@gmail.com>
2024-05-05 08:05:57 +0530

    getGraphsData method updated

--- backend/src/main/java/com/companyA/backend/GeneralManagementSystem/service/DashboardService.java
+++ backend/src/main/java/com/companyA/backend/GeneralManagementSystem/service/DashboardService.java
@@ -3,7 +3,9 @@ package com.companyA.backend.GeneralManagementSystem.service;
 import com.companyA.backend.GeneralManagementSystem.DTO.GraphsDataDTO;
 import com.companyA.backend.GeneralManagementSystem.DTO.InventoryDataDTO;
 import com.companyA.backend.InventoryManagementSystem.model.Stocks;
+import com.companyA.backend.InventoryManagementSystem.model.Warehouse;
 import com.companyA.backend.InventoryManagementSystem.repository.InventoryRepository;
+import com.companyA.backend.InventoryManagementSystem.repository.WarehouseRepository;
 import com.companyA.backend.LogisticsAndMaintenanceSystem.repository.VehicleRepository;
 import com.companyA.backend.SalesSystem.model.FinanceSalesTableBody;
 import com.companyA.backend.SalesSystem.repository.FinanceSalesTableRepository;
@@ -20,23 +22,41 @@ public class DashboardService {
 
     private final InventoryRepository<Stocks> inventoryRepository;
     private final FinanceSalesTableRepository financeSalesTableRepository;
+    private final WarehouseRepository warehouseRepository;
+
 
     public GraphsDataDTO getGraphsData() {
         GraphsDataDTO graphsDataDTO = new GraphsDataDTO();
 
-        List<Stocks> inventoryExistList = inventoryRepository.findAll();
-        List<FinanceSalesTableBody> financeSalesTableBodyExistList = financeSalesTableRepository.findAll();
+        try {
+            // Fetching the current data
+            List<Stocks> inventoryExistList = inventoryRepository.findAll();
+            List<FinanceSalesTableBody> financeSalesTableBodyExistList = financeSalesTableRepository.findAll();
+            List<Warehouse> stocksInEachWareHouseExistList = warehouseRepository.findAll();
 
-        Map<String, Integer> inventoryFilteredData = inventoryExistList.stream()
-                .map(this::mapToDTO)
-                .collect(Collectors.toMap(InventoryDataDTO::getName, InventoryDataDTO::getQuantity));
+            // Data Fetching from Inventory
+            Map<String, Integer> inventoryFilteredData = inventoryExistList.stream()
+                    .map(this::mapToDTO)
+                    .collect(Collectors.toMap(InventoryDataDTO::getName, InventoryDataDTO::getQuantity));
 
-        Map<String, Double> totalOrderAmountPerDay = financeSalesTableBodyExistList.stream()
-                .collect(Collectors.groupingBy(FinanceSalesTableBody::getOrder_date,
-                        Collectors.summingDouble(FinanceSalesTableBody::getOrder_amount)));
+            // Data Fetching from Sales
+            Map<String, Double> totalOrderAmountPerDay = financeSalesTableBodyExistList.stream()
+                    .collect(Collectors.groupingBy(FinanceSalesTableBody::getOrder_date,
+                            Collectors.summingDouble(FinanceSalesTableBody::getOrder_amount)));
 
-        graphsDataDTO.setInventoryData(inventoryFilteredData);
-        graphsDataDTO.setSalesData(totalOrderAmountPerDay);
+            // Data Fetching from warehouses
+            Map<String, Integer> stocksInEachWareHouse = stocksInEachWareHouseExistList.stream()
+                    .collect(Collectors.toMap(Warehouse::getName, warehouse -> warehouse.getInventoryList().size()));
+
+
+            graphsDataDTO.setInventoryData(inventoryFilteredData);
+            graphsDataDTO.setSalesData(totalOrderAmountPerDay);
+            graphsDataDTO.setWarehouseData(stocksInEachWareHouse);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return graphsDataDTO;
     }
 
