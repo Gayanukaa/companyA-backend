fb6f506ce4b06ae65bdf2d9dbc6526e8193d74c7
navinud <166421564+navinud@users.noreply.github.com>
2024-05-05 18:02:43 +0530

    employee corrected

---
+++ backend/src/main/java/com/companyA/backend/ManufacturingSystem/model/Employee.java
@@ -0,0 +1,19 @@
+package com.companyA.backend.ManufacturingSystem.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Employee")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    private String id;              // Unique identifier for the employee
+    private String firstName;       // First name of the employee
+    private String lastName;        // Last name of the employee
+    private String department;      // Department to which the employee belongs
+
+
+}
